3e6e4a96b4310048844f70dc43555b5109e9d9b8
unio <unioblog@gmail.com>
2018-10-04 08:47:58 +0900

    learn props of components

--- src/App.js
+++ src/App.js
@@ -12,16 +12,31 @@ import React from 'react';
 //  }
 //}
 
+const profiles = [
+  {name: "Taro", age: 10},
+  {name: "Hanako", age: 5},
+  {name: "Unio", age: 28},
+  {name: "Noname"}
+]
+
 const App = () => {
   return (
   <div>
-    <User />
+    {
+      profiles.map((profile,index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
   </div>
   )
 }
 
-const User = () => {
-  return <div>Hi!</div>
+const User = (props) => {
+  return <div>Hi!I am {props.name}, and {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
