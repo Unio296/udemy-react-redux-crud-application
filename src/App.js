34e1a5aefde984464ff8dc2f0a1ba4cf01dfbc63
unio <unioblog@gmail.com>
2018-10-04 08:37:36 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-//    const greeting = "Hi, Unio!"
-//    const dom = <h1 className="foo">{greeting}</h1>
-//    return <input type="text" onClick={() => {console.log("clicked.")}} />;
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("clicked.")}} />
-      </React.Fragment>
-    );
+//class App extends Component {
+//  render() {
+//    return (
+//      <React.Fragment>
+//        <label htmlFor="bar">bar</label>
+//        <input type="text" onClick={() => {console.log("clicked.")}} />
+//      </React.Fragment>
+//    );
+//
+//  }
+//}
 
-  }
+const App = () => {
+  return (
+  <div>
+    <User />
+  </div>
+  )
+}
+
+const User = () => {
+  return <div>Hi!</div>
 }
 
 export default App;
