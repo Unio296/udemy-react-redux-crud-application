d1afc2798318aa325aff9721f07750ce4ad83c56
unio <unioblog@gmail.com>
2018-10-04 21:02:49 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //class App extends Component {
 //  render() {
@@ -35,8 +36,15 @@ const User = (props) => {
   return <div>Hi!I am {props.name}, and {props.age} years old.</div>
 }
 
-User.defaultProps = {
-  age: 1
+//値を定義しない場合デフォルトで入る値を設定できる
+//User.defaultProps = {
+//  age: 1
+//}
+
+//型のチェックができる
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 export default App;
